377792d1ff2d5392d80375bc7b21dc964d452bce
Tony Di Croce <dicroce@gmail.com>
2025-02-13 13:08:04 -0500

    fixed build

--- find_libunwind.cmake
+++ find_libunwind.cmake
@@ -1,69 +1,40 @@
-# This file downloaded from https://raw.githubusercontent.com/m-a-d-n-e-s-s/madness/master/cmake/modules/FindLibunwind.cmake
-# - Try to find Libunwind
-# Input variables:
-#  LIBUNWIND_ROOT_DIR     - The libunwind install directory
-#  LIBUNWIND_INCLUDE_DIR  - The libunwind include directory
-#  LIBUNWIND_LIBRARY      - The libunwind library directory
-# Output variables:
-#  LIBUNWIND_FOUND        - System has libunwind
-#  LIBUNWIND_INCLUDE_DIRS - The libunwind include directories
-#  LIBUNWIND_LIBRARIES    - The libraries needed to use libunwind
-#  LIBUNWIND_VERSION      - The version string for libunwind
+# - Try to find libunwind
+# Once done this will define
+#
+#  LIBUNWIND_FOUND - system has libunwind
+#  LIBUNWIND_INCLUDE_DIR - the libunwind include directory
+#  LIBUNWIND_LIBRARIES - Link these to use libunwind
+#  LIBUNWIND_DEFINITIONS - Compiler switches required for using libunwind
+
+# Copyright (c) 2006, Alexander Dymo, <adymo@kdevelop.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+find_path(LIBUNWIND_INCLUDE_DIR libunwind.h
+  /usr/include
+  /usr/local/include
+)
+
+find_library(LIBUNWIND_LIBRARIES NAMES unwind )
+if(NOT LIBUNWIND_LIBRARIES STREQUAL "LIBUNWIND_LIBRARIES-NOTFOUND")
+  if (CMAKE_COMPILER_IS_GNUCC)
+    set(LIBUNWIND_LIBRARIES "gcc_eh;${LIBUNWIND_LIBRARIES}")
+  endif()
+endif()
+
+# some versions of libunwind need liblzma, and we don't use pkg-config
+# so we just look whether liblzma is installed, and add it if it is.
+# It might not be actually needed, but doesn't hurt if it is not.
+# We don't need any headers, just the lib, as it's privately needed.
+message(STATUS "looking for liblzma")
+find_library(LIBLZMA_LIBRARIES lzma )
+if(NOT LIBLZMA_LIBRARIES STREQUAL "LIBLZMA_LIBRARIES-NOTFOUND")
+  message(STATUS "liblzma found")
+  set(LIBUNWIND_LIBRARIES "${LIBUNWIND_LIBRARIES};${LIBLZMA_LIBRARIES}")
+endif()
 
 include(FindPackageHandleStandardArgs)
-  
-if(NOT DEFINED LIBUNWIND_FOUND)
-
-  # Set default sarch paths for libunwind
-  if(LIBUNWIND_ROOT_DIR)
-    set(LIBUNWIND_INCLUDE_DIR ${LIBUNWIND_ROOT_DIR}/include CACHE PATH "The include directory for libunwind")
-    if(CMAKE_SIZEOF_VOID_P EQUAL 8 AND CMAKE_SYSTEM_NAME STREQUAL "Linux")
-      set(LIBUNWIND_LIBRARY ${LIBUNWIND_ROOT_DIR}/lib64;${LIBUNWIND_ROOT_DIR}/lib CACHE PATH "The library directory for libunwind")
-    else()
-      set(LIBUNWIND_LIBRARY ${LIBUNWIND_ROOT_DIR}/lib CACHE PATH "The library directory for libunwind")
-    endif()
-  endif()
-  
-  find_path(LIBUNWIND_INCLUDE_DIRS NAMES libunwind.h
-      HINTS ${LIBUNWIND_INCLUDE_DIR})
-  
-  find_library(LIBUNWIND_LIBRARIES unwind 
-      HINTS ${LIBUNWIND_LIBRARY})
-  
-  # Get libunwind version
-  if(EXISTS "${LIBUNWIND_INCLUDE_DIRS}/libunwind-common.h")
-    file(READ "${LIBUNWIND_INCLUDE_DIRS}/libunwind-common.h" _libunwind_version_header)
-    string(REGEX REPLACE ".*define[ \t]+UNW_VERSION_MAJOR[ \t]+([0-9]+).*" "\\1" 
-        LIBUNWIND_MAJOR_VERSION "${_libunwind_version_header}")
-    string(REGEX REPLACE ".*define[ \t]+UNW_VERSION_MINOR[ \t]+([0-9]+).*" "\\1"
-        LIBUNWIND_MINOR_VERSION "${_libunwind_version_header}")
-    string(REGEX REPLACE ".*define[ \t]+UNW_VERSION_EXTRA[ \t]+([0-9]*).*" "\\1"
-        LIBUNWIND_MICRO_VERSION "${_libunwind_version_header}")
-    if(LIBUNWIND_MICRO_VERSION)
-      set(LIBUNWIND_VERSION "${LIBUNWIND_MAJOR_VERSION}.${LIBUNWIND_MINOR_VERSION}.${LIBUNWIND_MICRO_VERSION}")
-    else()
-      set(LIBUNWIND_VERSION "${LIBUNWIND_MAJOR_VERSION}.${LIBUNWIND_MINOR_VERSION}")
-    endif()
-    unset(_libunwind_version_header)
-  endif()
-
-  # handle the QUIETLY and REQUIRED arguments and set LIBUNWIND_FOUND to TRUE
-  # if all listed variables are TRUE
-  find_package_handle_standard_args(Libunwind
-      FOUND_VAR LIBUNWIND_FOUND
-      VERSION_VAR LIBUNWIND_VERSION 
-      REQUIRED_VARS LIBUNWIND_LIBRARIES LIBUNWIND_INCLUDE_DIRS)
-
-  mark_as_advanced(LIBUNWIND_INCLUDE_DIR LIBUNWIND_LIBRARY 
-      LIBUNWIND_INCLUDE_DIRS LIBUNWIND_LIBRARIES)
-
-endif()
-
-if(LIBUNWIND_FOUND)
-  if(NOT TARGET LIBUNWIND::LIBUNWIND)
-    add_library(LIBUNWIND::LIBUNWIND UNKNOWN IMPORTED)
-    set_target_properties(LIBUNWIND::LIBUNWIND PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${LIBUNWIND_INCLUDE_DIRS}"
-        IMPORTED_LOCATION "${LIBUNWIND_LIBRARIES}" )
-  endif()
-endif()
+find_package_handle_standard_args(Libunwind "Could not find libunwind" LIBUNWIND_INCLUDE_DIR LIBUNWIND_LIBRARIES)
+# show the LIBUNWIND_INCLUDE_DIR and LIBUNWIND_LIBRARIES variables only in the advanced view
+mark_as_advanced(LIBUNWIND_INCLUDE_DIR LIBUNWIND_LIBRARIES )
